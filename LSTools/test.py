8afe27bdea40181ef0e440b064b8ab556ce9dd65
Therzyx <2274466264@qq.com>
2024-03-13 22:26:55 +0800

    update qt5ui

--- LSTools/test.py
+++ LSTools/test.py
@@ -1,16 +1,26 @@
-def generate_control_xy():
-    start_x = 50
-    start_y = 100
-    while True:
-        yield (start_x, start_y)
-        if start_x + 120 >= 800:
-            start_y += 70
-            start_x = 50
-        else:
-            start_x += 170
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from tools_view import Ui_mainWindow
+from tools.studio_control import *
 
 
-if __name__ == '__main__':
-    gen = generate_control_xy()
-    for _ in range(5):
-        print(next(gen))
+class MainWindow(QMainWindow):
+    def __init__(self, bts: list):
+        super(MainWindow, self).__init__()
+        self.ui = Ui_mainWindow(bts)
+        self.ui.setupUi(self)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    bts = [
+        {"text": "日志文件夹", "func": open_logs_dir},
+        {"text": "多语言文件夹", "func": open_logs_dir},
+        {"text": "branch切换", "func": open_logs_dir},
+        {"text": "清除美颜", "func": open_logs_dir},
+        {"text": "清除场景", "func": open_logs_dir},
+        {"text": "生成完成PK参数", "func": open_logs_dir}
+    ]
+    mainWindow = MainWindow(bts=bts)
+    mainWindow.show()
+    sys.exit(app.exec_())
