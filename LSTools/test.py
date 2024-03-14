e0e3e99045493ee918c6d986bdbdd067b9858b72
Therzyx <therzyx@gmail.com>
2024-03-14 20:29:08 +0800

    update ui

--- LSTools/test.py
+++ LSTools/test.py
@@ -15,11 +15,11 @@ if __name__ == "__main__":
     app = QApplication(sys.argv)
     bts = [
         {"text": "日志文件夹", "func": open_logs_dir},
-        {"text": "多语言文件夹", "func": open_logs_dir},
-        {"text": "branch切换", "func": open_logs_dir},
-        {"text": "清除美颜", "func": open_logs_dir},
-        {"text": "清除场景", "func": open_logs_dir},
-        {"text": "生成完成PK参数", "func": open_logs_dir}
+        {"text": "多语言文件夹", "func": open_language_dir},
+        {"text": "branch切换", "func": switch_branch},
+        {"text": "清除美颜", "func": update_effects},
+        {"text": "清除场景", "func": clear_screen},
+        {"text": "生成完成PK参数", "func": generate_finsh_pk}
     ]
     mainWindow = MainWindow(bts=bts)
     mainWindow.show()
--- LSTools/tools_view.py
+++ LSTools/tools_view.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import sys
 from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtGui import QFont
 from PyQt5.QtWidgets import QApplication, QDesktopWidget
 
 
@@ -9,8 +10,8 @@ class Ui_mainWindow(object):
         # 获取系统分辨率
         screen_width, screen_height = self.sys_resolution if self.sys_resolution != "" else (1920, 1080)
         # 获取比例
-        self.with_scale = screen_width / 1920
-        self.height_scale = screen_height / 1080
+        self.with_scale = int(screen_width / 1920)
+        self.height_scale = int(screen_height / 1080)
         self.bts_info = bts_info
 
     @property
@@ -27,22 +28,6 @@ class Ui_mainWindow(object):
                 return int(resolution[0]), int(resolution[1])
         return resolution
 
-    def setupUi(self, mainWindow):
-        mainWindow.setObjectName("mainWindow")
-        mainWindow.resize(800, 500)
-        mainWindow.setToolButtonStyle(QtCore.Qt.ToolButtonFollowStyle)
-        self.centralwidget = QtWidgets.QWidget(mainWindow)
-        self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(50, 30, 700, 40))
-        self.bts = []
-        generate_xy = self.generate_control_xy()
-        for i, _ in enumerate(self.bts_info):
-            x, y = next(generate_xy)
-            self.bts.append(QtWidgets.QPushButton(self.centralwidget))
-            self.bts[i].setGeometry(QtCore.QRect(x, y, 120, 40))
-        mainWindow.setCentralWidget(self.centralwidget)
-        self.retranslateUi(mainWindow)
-
     def generate_control_xy(self):
         start_x = 50
         start_y = 100
@@ -54,13 +39,33 @@ class Ui_mainWindow(object):
             else:
                 start_x += 190
 
+    def show_result(self, func):
+        result = func()
+        self.toast = str(result)
+
+    def setupUi(self, mainWindow):
+        mainWindow.setObjectName("mainWindow")
+        mainWindow.resize(800 * self.with_scale, 500 * self.height_scale)
+        mainWindow.setToolButtonStyle(QtCore.Qt.ToolButtonFollowStyle)
+        self.centralwidget = QtWidgets.QWidget(mainWindow)
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(50, 30, 700, 40))
+        self.bts = []
+        generate_xy = self.generate_control_xy()
+        for i, _ in enumerate(self.bts_info):
+            x, y = next(generate_xy)
+            self.bts.append(QtWidgets.QPushButton(self.centralwidget))
+            self.bts[i].setGeometry(QtCore.QRect(x, y, 120 * self.with_scale, 40 * self.height_scale))
+        mainWindow.setCentralWidget(self.centralwidget)
+        self.retranslateUi(mainWindow)
+
     def retranslateUi(self, mainWindow):
         _translate = QtCore.QCoreApplication.translate
         mainWindow.setWindowTitle(_translate("mainWindow", "Tools"))
         self.label.setText(_translate("mainWindow", "TextLabel"))
+        font = QFont()
+        font.setPointSize(10)  # 设置字体大小为16
         for i, bt in enumerate(self.bts):
             bt.setText(_translate("mainWindow", self.bts_info[i]["text"]))
-
-
-if __name__ == '__main__':
-    pass
+            bt.setFont(font)
+            bt.clicked.connect(self.bts_info[i]["func"])
